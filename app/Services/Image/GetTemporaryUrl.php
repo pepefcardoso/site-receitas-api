3c9285f741f12f02b73bfe92a8007dfc8603b691
pepefcardoso <pppfcardoso@gmail.com>
2025-04-03 15:12:54 -0300

    feat: added cache to get image url service

--- app/Services/Image/GetTemporaryUrl.php
+++ app/Services/Image/GetTemporaryUrl.php
@@ -3,6 +3,7 @@
 namespace App\Services\Image;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Image;
 
@@ -10,13 +11,19 @@ class GetTemporaryUrl
 {
     public function temporaryUrl(Image $image): string
     {
-        try {
-            return Storage::disk('s3')->temporaryUrl(
+        $cacheKey = 'image_url_' . $image->id;
+        $cacheTtl = 5;
+
+        $url = Cache::get($cacheKey);
+
+        if (!$url) {
+            $url = Storage::disk('s3')->temporaryUrl(
                 $image->path,
-                Carbon::now()->addMinutes(5)
+                Carbon::now()->addMinutes($cacheTtl)
             );
-        } catch (\Exception $e) {
-            throw $e;
+            Cache::put($cacheKey, $url, now()->addMinutes($cacheTtl));
         }
+
+        return $url;
     }
 }
