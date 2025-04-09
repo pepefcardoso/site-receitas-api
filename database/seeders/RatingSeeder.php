5d308ba59f409d05b2918698f5b31324c7853753
pepefcardoso <pppfcardoso@gmail.com>
2025-04-09 13:49:45 -0300

    feat: updating ratings seeder and factory

--- database/seeders/RatingSeeder.php
+++ database/seeders/RatingSeeder.php
@@ -2,16 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Recipe;
 use App\Models\Rating;
 use Illuminate\Database\Seeder;
 
 class RatingSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Rating::factory(10)->create();
+        $users = range(1, 11);
+        $posts = Post::pluck('id');
+        $recipes = Recipe::pluck('id');
+
+        foreach ($users as $userId) {
+            foreach ($posts as $postId) {
+                Rating::factory()->create([
+                    'user_id' => $userId,
+                    'rateable_id' => $postId,
+                    'rateable_type' => 'App\Models\Post',
+                ]);
+            }
+
+            foreach ($recipes as $recipeId) {
+                Rating::factory()->create([
+                    'user_id' => $userId,
+                    'rateable_id' => $recipeId,
+                    'rateable_type' => 'App\Models\Recipe',
+                ]);
+            }
+        }
     }
 }
