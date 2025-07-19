8016c668525a3bb463b37ebfe0a92d50f1c6f385
pepefcardoso <pppfcardoso@gmail.com>
2025-07-19 16:07:39 -0300

    feat: installed sentry

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,11 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'sentry' => [
+        'driver' => 'sentry',
+        'level' => 'error',
+    ],
+
     ],
 
 ];
