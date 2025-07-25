f049c5ff4881f537fffd13e96bd6f2df31ebe6ea
pepefcardoso <pppfcardoso@gmail.com>
2025-07-25 14:17:19 -0300

    updating bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,14 +8,25 @@ use Illuminate\Support\Facades\RateLimiter;
 use App\Http\Middleware\SecureHeaders;
 use Illuminate\Http\Request;
 
-require __DIR__ . '/../vendor/autoload.php';
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            RateLimiter::for(
+                'api',
+                fn(Request $request) =>
+                Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())
+            );
+
+            RateLimiter::for(
+                'auth',
+                fn(Request $request) =>
+                Limit::perMinute(5)->by($request->ip())
+            );
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
@@ -25,18 +36,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(append: [
             SecureHeaders::class,
         ]);
-
-        RateLimiter::for(
-            'api',
-            fn(Request $request) =>
-            Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())
-        );
-
-        RateLimiter::for(
-            'auth',
-            fn(Request $request) =>
-            Limit::perMinute(5)->by($request->ip())
-        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
