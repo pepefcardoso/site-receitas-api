343d108a155afd83f87c6374e06a7538e60ae20c
pepefcardoso <pppfcardoso@gmail.com>
2025-08-05 14:23:31 -0300

    FIX: REMOVING UNUSED PACKAGES AND FIXING PATHS

--- config/services.php
+++ config/services.php
@@ -14,9 +14,9 @@ return [
     |
     */
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+    // 'postmark' => [
+    //     'token' => env('POSTMARK_TOKEN'),
+    // ],
 
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
@@ -24,16 +24,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'resend' => [
-        'key' => env('RESEND_KEY'),
-    ],
+    // 'resend' => [
+    //     'key' => env('RESEND_KEY'),
+    // ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
+    // 'slack' => [
+    //     'notifications' => [
+    //         'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+    //         'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+    //     ],
+    // ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
